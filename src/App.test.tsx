edbca34b98ba0a41beba34ab9a116bab835e7d12
Joshua Rogan <joshua.rogan@mydit.ie>
2021-04-07 20:34:58 +0100

    broke test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,6 +5,6 @@ import "@testing-library/jest-dom/extend-expect";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn angular/i);
   expect(linkElement).toBeInTheDocument();
 });
